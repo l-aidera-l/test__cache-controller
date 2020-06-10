fa4fe47938a8115a3c8eda5b9a0dd445661267fa
Evgenia <hope.aidera@gmail.com>
2020-06-10 13:09:11 +0300

    Updates

---
+++ precache-manifest.05718da9ebfb92db6d3ba7e93801cbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16df00e11b5c98342526426a144cd0dc",
+    "url": "/test__cache-controller/index.html"
+  },
+  {
+    "revision": "75c9223b5a306de78c6f",
+    "url": "/test__cache-controller/static/css/main.41ee0ca3.chunk.css"
+  },
+  {
+    "revision": "f9fd7858cdcc24c39710",
+    "url": "/test__cache-controller/static/js/2.57410717.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/test__cache-controller/static/js/2.57410717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c9223b5a306de78c6f",
+    "url": "/test__cache-controller/static/js/main.d6166291.chunk.js"
+  },
+  {
+    "revision": "007c613e0af1e25c0cc2",
+    "url": "/test__cache-controller/static/js/runtime-main.de96640c.js"
+  }
+]);
